9b7e5265ad3ea3bdfe25280e55e161ed2c22dfe6
Arunkumar-60 <pikkalaarunkumar@gmail.com>
2026-07-27 19:45:01 +1000

    Initial Flask To-Do App setup

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    data = {'message': 'this is json response'}
+    return jsonify(data)
+
+if __name__=='__main__':
+    app.run(debug=True)
