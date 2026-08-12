f50d47b08e30bc9bca44d3ebf7fb165511b592ed
Arunkumar-60 <pikkalaarunkumar@gmail.com>
2026-08-12 23:30:39 +1000

    commit

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, jsonify
-
+import json
 app = Flask(__name__)
 
+notes = [{'id':1,'title':'title1', 'des':'descripton of first note'},{'id':2,'title':'title2','des':'description of second note'}]
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/notes')
+def get_notes():
+    return render_template('notes.html',notes=notes)
 
 @app.route('/data', methods=['GET'])
 def get_data():
